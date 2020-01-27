8fe86e15ddc0361bb3ba6af094545a55990685d4
Vince Chan <vincecancode@gmail.com>
2020-01-27 17:10:57 +0800

    Updated AMI images for AZs

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -1,17 +1,25 @@
 resource "aws_key_pair" "mykey" {
   key_name   = "mykey"
+  # public key content.
+  # function "file" is used to load the content of the key from the path
+  # Important: The key has to be created/existed in the parth before the terraform apply
+  #            The way to create the key : ssh-keygen -f mykey
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+  # The public key to be imported to the target instance, used for remote access
   key_name      = aws_key_pair.mykey.key_name
 
+  # The file to be uploaded to the target instance, where source is in relative location of this script
   provisioner "file" {
     source      = "script.sh"
     destination = "/tmp/script.sh"
   }
+
+  # Call the remote execution of the script after the system boots
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/script.sh",
