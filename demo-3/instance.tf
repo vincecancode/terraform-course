9811469137a74d267968184f6851dc9800204258
Vince Chan <vincecancode@gmail.com>
2020-01-27 20:44:12 +0800

    Updated AMI images for AZs

--- demo-3/instance.tf
+++ demo-3/instance.tf
@@ -1,11 +1,14 @@
 resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+  # Following is the script which will run locally (not at the target server),
+  # so in this example, we can use the private_ips.txt to construct the host file for ansible use.
   provisioner "local-exec" {
     command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
   }
 }
 
+# For the following. it will show in the Output section like "ip = 52.212.28.166"
 output "ip" {
   value = aws_instance.example.public_ip
 }
